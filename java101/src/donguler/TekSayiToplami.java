8cfe28b2f0e4d5cbdfa9dd5db9a37a82cd583dfd
Betül Şahin <betulsahin3387@hotmail.com>
2021-07-05 19:36:56 +0300

    tek sayi toplami alistirmasi

--- java101/src/donguler/TekSayiToplami.java
+++ java101/src/donguler/TekSayiToplami.java
@@ -1,2 +1,20 @@
-package donguler;public class TekSayiToplami {
+package donguler;
+
+import java.util.Scanner;
+
+public class TekSayiToplami {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sayi;
+        int sum = 0;
+        do{
+            System.out.print("Sayi giriniz : ");
+            sayi = scanner.nextInt();
+            if(sayi % 4 == 0){
+                sum += sayi;
+            }
+        }while(sayi % 2 == 0 );
+
+        System.out.println("Toplami : " + sum);
+    }
 }
