39375c17a2597a8a6a1f25f70666d8e9f755e7ed
Betül Şahin <betulsahin3387@hotmail.com>
2021-07-05 19:46:11 +0300

    girilen sayıya kadar olan 4 ve 5'in kuvvetlerini ekrana yazdıran program

--- java101/src/donguler/Dongu3.java
+++ java101/src/donguler/Dongu3.java
@@ -1,2 +1,26 @@
-package donguler;public class Dongu3 {
+package donguler;
+
+import java.util.Scanner;
+
+/**
+ * Problem: Java döngüler ile girilen sayıya kadar olan 4 ve 5'in kuvvetlerini ekrana yazdıran program.
+ */
+public class Dongu3 {
+    public static int kuvvetiniAl(int x, int n){
+        int kuvveti = 1;
+        for(int i=0; i<n; i++){
+            kuvveti *= x;
+        }
+        return kuvveti;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Kuvvet giriniz : ");
+        int kuvvet = scan.nextInt();
+
+        System.out.println("4'ün " + kuvvet + ". kuvveti = " + kuvvetiniAl(4, kuvvet));
+        System.out.println("4'ün " + kuvvet + ". kuvveti = " + kuvvetiniAl(5, kuvvet));
+
+    }
 }
